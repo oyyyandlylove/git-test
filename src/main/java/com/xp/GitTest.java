40d523db893ba1def51a24e5df582baac7560ed3
oyyyandlylove <116674775+oyyyandlylove@users.noreply.github.com>
2023-02-25 11:07:24 +0800

    pull test

--- src/main/java/com/xp/GitTest.java
+++ src/main/java/com/xp/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("master update");
         System.out.println("hot-fix update");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
